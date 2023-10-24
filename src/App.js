54bd18ea413f7262ae7c4bc5b4e42585774537a8
seoalirajpoot <seo.alirajpoot@gmail.com>
2023-10-25 03:23:47 +0500

    btns

--- src/App.js
+++ src/App.js
@@ -120,7 +120,7 @@ function App() {
                 <Route exact element={<Home />} path='/' />
                 <Route exact element={<UserPanel />} path='/user-profile/:userId' />
                 <Route exact path='/single_Add/:productId' element={<SingleAdd />} />
-                <Route exact path='/Products/' element={<Products />} />
+                <Route exact path='/Products' element={<Products />} />
                 <Route exact path='/Products/:prodctName' element={<Products />} />
                 <Route exact path='/cart' element={<Cart />} />
                 <Route exact path='/cart/:userId' element={<Cart />} />
