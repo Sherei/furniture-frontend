f97ee96afec91d4f73a500f8df41b4fbe9b1d4ba
seoalirajpoot <seo.alirajpoot@gmail.com>
2024-01-26 15:25:03 +0500

    singel

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -6,15 +6,15 @@ const Loader = () => {
         <div className='d-flex gap-4 px-4 py-2 align-items-center' style={{
             width: "fit-contet",
             height: "fit-content",
-            boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+            // boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",..
             zIndex: "100"
         }}>
             <div>
                 <ClipLoader color="#1b2950" />
             </div>
-            <div>
+            {/* <div>
                 <p className='text-muted m-0'>Loading! Please Wait </p>
-            </div>
+            </div> */}
 
         </div>
 
