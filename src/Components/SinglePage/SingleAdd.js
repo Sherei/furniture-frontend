a47e9eedb147f7dc5b0b79881349c8a75f6e4a52
seoalirajpoot <seo.alirajpoot@gmail.com>
2024-02-04 22:03:25 +0500

    add blog

--- src/Components/SinglePage/SingleAdd.js
+++ src/Components/SinglePage/SingleAdd.js
@@ -685,7 +685,7 @@ const SingleAdd = () => {
                     </div>
                   }
 
-                  {(product.category === "sofa" && product.category === "footstools" && product.color1 != undefined) &&
+                  {((product.category === "sofa" || product.category === "footstools") && product.color1 != undefined) &&
                     <div className="mt-1">
                       <label
                         style={{ fontSize: "17px", fontWeight: "600" }}
