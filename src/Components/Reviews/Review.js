10a4485b9030e18a79cb3d503ffd3c1b1f7ab1cd
seoalirajpoot <seo.alirajpoot@gmail.com>
2024-02-04 12:16:19 +0500

    pic

--- src/Components/Reviews/Review.js
+++ src/Components/Reviews/Review.js
@@ -128,7 +128,7 @@ const Review = () => {
                                                 {item.comment}
                                             </p>
                                             <p className='text-center' style={{ color: "white" }}>{item.name}</p>
-                                            {/* <p className='text-center text-muted' style={{ fontWeight: "700" }}>{formatDateTime(item.date1 ? item.date1 : item.date)}</p> */}
+                                          <p className='text-center text-muted' style={{ fontWeight: "700" }}>{formatDateTime(item.date1 ? item.date1 : item.date)}</p> 
                                         </div>
                                     </SwiperSlide>
                                 })}
