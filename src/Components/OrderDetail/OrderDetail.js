fd10ad6d828a872a0082c89e601423ed20283408
seoalirajpoot <seo.alirajpoot@gmail.com>
2024-02-09 00:33:11 +0500

    order

--- src/Components/OrderDetail/OrderDetail.js
+++ src/Components/OrderDetail/OrderDetail.js
@@ -50,9 +50,6 @@ const OrderDetail = () => {
             year: 'numeric',
             month: 'long',
             day: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric',
-            second: 'numeric',
         };
         const date = new Date(dateStr);
         return date.toLocaleDateString('en-GB', options);
