e56850e2330603b003f3e69f742a8f9bcafc0505
seoalirajpoot <seo.alirajpoot@gmail.com>
2024-02-09 02:37:07 +0500

    cart

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -308,11 +308,11 @@ const Checkout = () => {
                             </div>
                         </div>
                         <div className='py-3'>
-                            <p className='fs-6' style={{ fontWeight: "600", color: "rgb(27, 41, 80)" }}>Payment</p>
+                            <p className='fs-6' style={{ fontWeight: "600", color: "rgb(27, 41, 80)" }}>Payment Method</p>
                             <div className="col-md-12 mb-3">
                                 <div className='px-3 py-2 d-flex justify-content-between align-items-center  rounded-3'
                                     style={{ border: "1px solid lightgray" }}>
-                                    <p className='m-0'>Cash on Delivery (COD) &pound;50 Deposit Required</p>
+                                    <p className='m-0'>Cash on Delivery (COD)</p>
                                 </div>
                             </div>
                         </div>
@@ -409,7 +409,7 @@ const Checkout = () => {
 
                     <div className='row mt-3 py-3 border' style={{ backgroundColor: "white" }}>
                         <div className='px-3 pt-3 col-12  d-flex justify-content-between align-items-center'>
-                            <p className='fs-6'>Subtotal {totalQuantity > 1 && <span className='fw-bolder'>({totalQuantity})</span>}</p>
+                            <p className='fs-6'>Subtotal {totalQuantity > 1 && <span className=''>({totalQuantity})</span>}</p>
                             <p className='fs-6'>{`£${totalSum?.toFixed()}`}</p>
                         </div>
                         <div className='px-3 col-12 d-flex justify-content-between align-items-center'>
