b95031bfddb9edc1c4992f433bf315185f080d72
seoalirajpoot <seo.alirajpoot@gmail.com>
2024-02-08 14:17:04 +0500

    price

--- src/Components/Dashboard/Products.js
+++ src/Components/Dashboard/Products.js
@@ -111,8 +111,8 @@ export const Products = () => {
                           <td onClick={() => move("/single_Add/" + data._id)}>
                             <img src={data.images[0]} alt="No   network" style={{ maxWidth: '80px', height: '80px' }} />
                           </td>
-                          <td className='text-center'>{data.stock === undefined ? "No" : "Yes"}</td>
-                          <td className='text-center' style={{ color: "red" }}>{data.home === true ? "Home" : "No"}</td>
+                          <td className='text-center'>{data.stock}</td>
+                          <td className='text-center'>{data.home}</td>
                           <td><a href={"/single_Add/" + data._id}>{data.title}</a></td>
                           <td >{data.category}</td>
                           <td className='text-center'>
