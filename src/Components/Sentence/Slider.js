abc7eeb652ae3ca78f0fcf649e38e285cc79c86e
seoalirajpoot <seo.alirajpoot@gmail.com>
2024-02-19 23:30:32 +0500

    single

--- src/Components/Sentence/Slider.js
+++ src/Components/Sentence/Slider.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import './slider.css';
+import './slider.css'; // Assuming you have your CSS styles defined in slider.css
 
 const Slider = () => {
   return (
     <div className="container-fluid px-0 mx-0 py-3" style={{ backgroundColor: "#F7EEDD" }}>
       <div className="row">
         <div className="col-12 d-flex slider-container">
-          <div className="slide-text">
-          Up to 30% OFF! 
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          FAST DELIVERY WITHIN 05-07 DAYS!  
+          <div className="slide-text direction-left">
+            <div>
+              Up to 30% OFF! 
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              FAST DELIVERY WITHIN 05-07 DAYS!
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              QUALITY GUARRANTEED
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </div>
           </div>
         </div>
       </div>
