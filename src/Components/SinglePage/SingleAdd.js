6f481b09730ff82e46bd641b5833c2aec4d915e0
seoalirajpoot <seo.alirajpoot@gmail.com>
2024-03-01 17:25:06 +0500

    navbar

--- src/Components/SinglePage/SingleAdd.js
+++ src/Components/SinglePage/SingleAdd.js
@@ -1410,7 +1410,7 @@ const SingleAdd = () => {
                 color: "#1b2950",
                 fontWeight: "600",
               }}>Share Product</p>
-              <p className="m-0 fs-6 d-flex justify-content-center align-items-center cursor" onClick={() => handleShare("general")} style={{ width: "30px", height: "30px", backgroundColor: "rgb(200, 197, 189,0.5)", borderRadius: "100%" }}><FaShareSquare /></p>
+              <p className="m-0 fs-6 d-flex justify-content-center align-items-center cursor" onClick={() => handleShare("general")} style={{ width: "30px", height: "30px", backgroundColor: "rgb(200, 197, 189, 0.4)", borderRadius: "100%" }}><FaShareSquare /></p>
               <p className="m-0 fs-6 d-flex justify-content-center align-items-center cursor" onClick={() => handleShare("instagram")} style={{ width: "30px", height: "30px", background: "linear-gradient(115deg, #f9ce34, #ee2a7b, #6228d7)", color: "rgb(255, 255, 255)", borderRadius: "100%" }}><FaInstagram /></p>
               <p className="m-0 fs-6 d-flex justify-content-center align-items-center cursor" onClick={() => handleShare("tiktok")} style={{ width: "30px", height: "30px", background: "black", color: "white", borderRadius: "100%" }}><FaTiktok /></p>
               <p className="m-0 fs-6 d-flex justify-content-center align-items-center cursor" onClick={() => handleShare("facebook")} style={{ width: "30px", height: "30px", backgroundColor: "rgb(24, 119, 242)", color: "rgb(255, 255, 255)", borderRadius: "100%" }}><FaFacebook /></p>
